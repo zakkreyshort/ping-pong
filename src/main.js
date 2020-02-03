c23e51eb998934efe374a02488aca6bc390f4dbd
Joseph Wangemann and Zakkrey Short <joseph@>
2020-02-03 10:09:49 -0800

    create initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { pingPong } from './ping-pong';
+
+$(document).ready(function() {
+    $('#ping-pong-form').submit(function(event) {
+      event.preventDefault();
+      var goal = $('#goal').val();
+      var output = pingPong(goal);
+      output.forEach(function(element) {
+        $('#solution').append("<li>" + element + "</li>");
+      });
+    });
+  });
